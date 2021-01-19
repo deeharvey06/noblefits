f7318bae329b8324a05bb1c088015ad56fa99a85
Clifford Harvey <cliffordharvey@Cliffords-MacBook-Pro.local>
2021-01-18 21:17:59 -0800

    add CustomButtom

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import FormInput from '../forminput/FormInput';
+import CustomButton from '../customButton/CustomButton'
 
 import './signin.scss'
 
@@ -49,7 +50,9 @@ class SignIn extends Component {
             required
           />
 
-          <input type='submit' value='Submit Form' />
+          <CustomButton type='submit'>
+            Sign in
+          </CustomButton>
         </form>
       </div>
     );

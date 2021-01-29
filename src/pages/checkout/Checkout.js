9458810b3898d742d68cc1643b41f61c74fb188e
Clifford Harvey <cliffordharvey@Cliffords-MacBook-Pro.local>
2021-01-28 17:20:05 -0800

    update stripe integration and update favicon and title

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -2,6 +2,8 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
+import StripeCheckoutButton from "../../components/stripeButton/StripeButton";
+
 import CheckoutItem from "../../components/checkoutItem/CheckoutItem";
 
 import {
@@ -38,6 +40,12 @@ const CheckoutPage = ({ cartItems, total }) => {
       </div>
       {renderIartItems()}
       <div className="total">TOTAL: ${total}</div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/24 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };

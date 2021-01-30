be70c5641d127fe29927e1855354d292d43dd317
Clifford Harvey <cliffordharvey@Cliffords-MacBook-Pro.local>
2021-01-29 22:49:34 -0800

    node-sass

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Directory from "../../components/directory/Directory";
 
-// import "./homePage.scss";
+import "./homePage.scss";
 
 const HomePage = () => {
   return (
